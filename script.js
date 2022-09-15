f2948a97db5039448462818c1d30bdefb3ad62e4
jennananas <jennarj97@gmail.com>
2022-09-16 00:03:45 +0200

    add setNewGrid() to choose size

--- script.js
+++ script.js
@@ -1,8 +1,38 @@
-const grid = document.querySelector(".grid-container")
-const gridSize = 256;
+const grid = document.querySelector('.grid-container')
 
-for (let i=0; i<gridSize; i++) {
-    const gridElement = document.createElement('div');
-    gridElement.classList.toggle('grid-element');
-    grid.appendChild(gridElement);
+function setNewGrid(gridSize){
+    grid.style.cssText = `grid-template-columns: repeat(${gridSize}, 1fr); 
+                          grid-template-rows: repeat(${gridSize}, 1fr);`;
+
+    console.log(grid.hasChildNodes())
+    if (grid.hasChildNodes()){
+        while(grid.hasChildNodes()){
+            grid.firstChild.remove();
+        }
+    }
+    for (let i=0; i<gridSize**2; i++) {
+        const gridElement = document.createElement('div');
+        gridElement.classList.toggle('grid-element');
+        
+        grid.appendChild(gridElement);
+
+    }
+
+    const pixels = document.querySelectorAll('.grid-element')
+    pixels.forEach((pixel) => pixel.addEventListener('mouseover', ()=> {
+    pixel.classList.add('hovered');
+    }))
 }
+
+function askSize(){
+    do {
+        gridSize = prompt("Grid size ? Enter a number <100 and >0");
+        
+    }
+    while (gridSize>=100 || gridSize<=0);
+    setNewGrid(gridSize);
+}
+
+setNewGrid(16)
+
+
